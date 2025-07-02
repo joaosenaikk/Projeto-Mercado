f4a317640426caf13931237a45d28b91743d98be
sistema.noturno <sistema.noturno@LAB-03-08.sistemafieto.rede>
2025-07-01 21:38:56 -0300

    criação do pacote DAO criação das classes cliente DAO

--- Mercado/src/application/Main.java
+++ Mercado/src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import java.sql.Connection;
 
 import connectionFactory.ConnectionDatabase;
+import dao.ClienteDAO;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import model.Cliente;
@@ -25,12 +26,25 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
-//		Connection con = ConnectionDatabase.getConnection();
-//		ConnectionDatabase.closeConnection(con);
+		Cliente cliente = new Cliente();
+		ClienteDAO clienteDAO = new ClienteDAO();
 		
+		cliente.setNomeCliente("Pedro");
+		cliente.setCpfCliente("1234567890");
+		cliente.setDataNasc("1900-09-09");
+		cliente.setEmail("Pedrodoscabal@gmail.com");
+		cliente.setEndereco("rua do cachimba");
+		cliente.setTelefone("12088887777");
+		
+		clienteDAO.create(cliente);
 
 		
 		
 		launch(args);
+	}
+
+	private static void launch(String[] args) {
+		// TODO Auto-generated method stub
+		
 	}
 }
